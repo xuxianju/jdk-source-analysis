bf89efd202545fd0de5669113d6675e68da7cfbe
xuxianju <77930740@qq.com>
2021-05-22 15:41:53 +0800

    OCT REM[jdk源码工程搭建]

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+import java.util.HashMap;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+
+        HashMap<String,Double> hashMap = new HashMap();
+        hashMap.put("key1",0.1);
+        hashMap.put("key2",0.2);
+    }
+}
